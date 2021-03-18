1a0b4715780a3d8016dfeba9d5fc827d24ddda31
ectaw6d <Andrew.Palet@McKesson.com>
2021-03-18 15:12:13 -0500

    completed the layout of the results page. Added navigation to the screen to go back in the app

---
+++ lib/screens/results_page.dart
@@ -0,0 +1,61 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+import '../components/reusable_card.dart';
+import 'package:bmi_calculator/components/bottom_calculate.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.center,
+              child: Text(
+                'Your Results',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              color: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Normal',
+                    style: kResultsTextStyle,
+                  ),
+                  Text(
+                    '18.3',
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'Your BMI is low, eat more',
+                    style: kBodyTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomCalculate(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            buttonTitle: 'RE-CACULATE',
+          ),
+        ],
+      ),
+    );
+  }
+}
